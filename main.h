d3f24dfafbfa9b8a85aa811d2a534b257eb916e7
SSabin0 <13078@holbertonstudents.com>
2026-04-16 14:11:18 +1000

    applying environ to main.h

--- main.h
+++ main.h
@@ -7,6 +7,14 @@
 #include <sys/wait.h>
 #include <string.h>
 
+extern char **environ;
+/**
+ * environ - Pointer to an array of strings that constitute the environment
+ *
+ * Description: Inherited from the parent process; used to access
+ * system variables and passed to execve when launching new programs.
+ */
+ 
 void print_prompt(void);
 
 #endif
