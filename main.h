25fe0cfe36e8bbf5645907c43acafd361b0a3a14
SSabin0 <13078@holbertonstudents.com>
2026-04-21 20:52:08 +1000

    getline & execve

--- main.h
+++ main.h
@@ -4,17 +4,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <sys/wait.h>
 #include <string.h>
+#include <signal.h>
+#include <errno.h>
 
 #define MAX_ARGS 64
 
 extern char **environ;
-/**
- * Description: Inherited from the parent process; used to access
- * system variables and passed to execve when launching new programs.
- */
- 
-void print_prompt(void);
 
-#endif
+/* shell.c */
+void print_prompt(void);
+char *trim_line(char *line);
+char **split_line(char *line, char **argv);
+void print_error(char *prog, int line_no, char *cmd);
+
+/* path.c */
+char *get_env(const char *name);
+char *find_path(char *cmd);
+
+/* builtins.c */
+int handle_builtin(char **argv, char *line, int last_status);
+
+/* main.c */
+int execute_command(char **argv, char *prog, int line_no);
+
+#endif /* MAIN_H */
