e66d935b4afaa39e03b6d314c5bc82d542069ebd
bumble-beed <shirleen.workstuff@gmail.com>
2026-05-08 08:01:08 +0000

    add fixes

--- builtins.c
+++ builtins.c
@@ -1,8 +1,19 @@
-int handle_builtin(char **argv, char *line, int last_status);
+#include "main.h"
 
-if (strcmp(argv[0], "exit") == 0)
+/**
+ * handle_builtin - checks and handles builtin commands
+ * @argv: argument array
+ * @line: input line buffer to free if exiting
+ * @last_status: last exit status
+ *
+ * Return: 1 if builtin was handled, 0 if not
+ */
+int handle_builtin(char **argv, char *line, int last_status);
 {
-    free(line);
-    exit(last_status);
+	if (strcmp(argv[0], "exit") == 0)
+	{
+		free(line);
+		exit(last_status);
+	}
+	return (0);
 }
-return (0);
